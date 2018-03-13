25f6a92a00c059621198b45e47d5363428afd6c4
bean <1052049021@qq.com>
2018-03-13 15:22:39 +0800

    v2.0

--- app/public/index.php
+++ app/public/index.php
@@ -1,19 +1,16 @@
 <?php
-
 header("Content-type:text/html; charset=utf-8");
 
-require __DIR__ . '/../../vendor/autoload.php';
+defined('PROJECT_PATH') or define('PROJECT_PATH', __DIR__ . '/../../'); // 项目目录
+defined('VENDOR_PATH') or define('VENDOR_PATH', __DIR__ . '/../../vendor/'); // 框架目录
+defined('APP_PATH') or define('APP_PATH', __DIR__ . '/../'); // 应用目录
+
+// 自动加载
+require VENDOR_PATH . 'autoload.php';
 
 // 框架主体
-require __DIR__ . '/../../vendor/beansir/newx-framework/Newx.php';
+require VENDOR_PATH . 'beansir/newx-framework/Newx.php';
 
-// 配置目录
-Newx::setDir('web', __DIR__); // 根目录
-Newx::setDir('app', __DIR__ . '/../'); // 应用目录
-Newx::setDir('module', __DIR__ . '/../../'); // 模块目录
-
-// 配置文件
-$config = require __DIR__ . '/../config/config.php';
-
-// 开始运行
+// go
+$config = require APP_PATH . 'config/config.php'; // 获取配置
 Newx::run($config);
